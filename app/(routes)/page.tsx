b0049f1f83bbe24149160c5aafa491b83bacb751
RifkiArifianto <rifkiarifianto@students.amikom.ac.id>
2025-02-25 09:37:47 +0700

    deploy

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBanner from "@/actions/get-banner";
+import getProducts from "@/actions/get-products";
+import Banner from "@/components/banner";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const banner = await getBanner("26144165-28e9-4714-bcfe-a1431437e79d");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Banner data={banner} />
+        <div className="flex flex-col gap-y-8 sm:px-6 lg:px-8">
+          <ProductList title="Produk Unggulan" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
